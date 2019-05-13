31781cc98ba901f8a832712419dfee5462381d3e
Jason Cho <jason.cho.86@gmail.com>
2019-05-13 10:55:00 -0400

    Defined Candy model and route for "api/candies"

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -3,4 +3,22 @@ const db = require('../database');
 
 module.exports = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    max: 10,
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    isUrl: true,
+  },
 });
